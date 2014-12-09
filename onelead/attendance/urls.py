44db7cfd5f8ed944b2d6a0cfe767dbed684129ee
Meera <meera@cosine>
2014-12-09 19:21:52 +0530

    Added modified attendance module.

--- onelead/attendance/urls.py
+++ onelead/attendance/urls.py
@@ -6,9 +6,6 @@ from attendance import views
 
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
-		url(r'^getslots', views.get_slots, name='get_slots'),
-		url(r'^addstudents', views.add_students, name='add_students'),
-		url(r'^submitstudents', views.submit_students, name='submit_students'),
-		url(r'^addbatch', views.submit_students, name='add_batch'),
+		url(r'^addattendance', views.add_attendance, name='add_attendance'),
 )+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- onelead/attendance/views.py
+++ onelead/attendance/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.template import RequestContext, loader
+
+# Create your views here.
+
 
 from django.http import HttpResponse
-
+from leaddb.models import Attendance
+from leaddb.models import Student
 
 
 def index(request):
@@ -9,46 +15,40 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'att.html', context)
 
-def get_slots(request):
-		latest_question_list = ['hello']
-		context = {'latest_question_list': latest_question_list}
-		return render(request, 'att.html', context)
 
-def add_students(request):
-		latest_question = ['hello']
-		context = {'latest_question': latest_question}
-		return render(request, 'add.html', context)
-
-
-#Save add student form url
-def submit_students(request):
+#Add attendance based on dojo ajax.
+def add_attendance(request):
 	if request.method == 'GET':
 		#Acessing form using a GET method , so deliver a form
-		template = loader.get_template('submit.html')
-		context = RequestContext(request)
+		student_list = Student.objects.all()
+		template = loader.get_template('add_attendance.html')
+		context = RequestContext(request,{'student_list': student_list})
 		return HttpResponse(template.render(context))
 	else:
-		#Acessing form using a POST method, so save the data and say "saved"
-		print request.POST['sname']
+		#Acessing form using a POST method(may be an ajax call} , so save the data and say "saved"
+		print request.POST
 		#create a Batch object and set variables and save
-		Student=Student()
-		Student.name=request.POST['sname']
-		Student.batch=request.POST['batch']
-		Student.gender=request.POST['sgender']
-		Student.address=request.POST['address']
-		Student.emergency_phone_no=request.POST['ephno']
-		Student.mobile_no=request.POST['mno']
-		Student.email=request.POST['semail']
-		Student.parents=request.POST['parents']
-		Student.education_history=request.POST['ehistory']
-		Student.admitted_date=request.POST['admndate']
-		Student.save()
+		attendance=Attendance()
+		attendance.time_slt=request.POST['empno']
+		attendance.student=Student.objects.filter(id=student_id)
+		staff.name=request.POST['name']
+		staff.gender=request.POST['gender']
+		staff.address=request.POST['address']
+		staff.emergency_phone_no=request.POST['ephno']
+		staff.mobile_no=request.POST['mno']
+		staff.email=request.POST['email']
+		staff.education_history=request.POST['ehistory']
+		staff.joined_date=request.POST['jdate']
+		staff.emp_type=request.POST['emptype']
+		staff.save()
 		
 		#retrieve all saved data and send bak to dojo
 		dbdata=""
-		students=Student.objects.all() #Retrieve all batches from db
-		for satch in students:
-			dbdata=dbdata+" "+Student.name
+		staffs=Staff.objects.all() #Retrieve all batches from db
+		for staff in staffs:
+			dbdata=dbdata+" "+staff.name
 		print dbdata
-		#Instead of this render the form again
 		return HttpResponse("Saved one, so total :"+dbdata)
+
+
+
